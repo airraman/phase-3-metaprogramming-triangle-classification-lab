fdcea6f30928f3b6093a16dce350863d09341c7f
Raman <ramanmcreates@gmail.com>
2022-11-26 14:07:57 -0500

    commit tree c945a0827a995dd14358663c3bd012ef6e8e292f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+    self.kind
+
+  end 
+
+  def kind
+    if (length1 == length2 && length2 == length3)
+       equilateral
+    elsif (length1 == length2 or length2 == length3 or length1 == length3 ) 
+       isosceles
+    elsif (length1 != length2 && length2 != length3 && length1 != length3 )
+       scalene
+    elsif (lenth1 == 0 or length2 == 0 or length3 == 0)
+      raise TriangleError
+    else 
+      begin 
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Please provide a valid triangle"
+    end
+  end
+
 end
+
+shape = Triangle.new(20, 20, 20)
+
+binding.pry
